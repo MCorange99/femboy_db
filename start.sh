7aaac399b7cefeb62e81a571a0b34dda444037a2
MCorange <mcorangecodes@gmail.com>
2023-02-05 17:29:51 +0200

    implememting CI

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# This script is only for server deployment!
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# load nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm run 19 .
